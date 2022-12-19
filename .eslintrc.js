570b1b432a4481d5913df32a8412c2ca8894ad55
Kamil Narkiewicz <kamil099887@gmail.com>
2022-12-19 16:55:32 +0100

    add eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,16 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'eslint-config-prettier'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['prettier', 'react-native'],
+  ignorePatterns: ['.eslintrc.js'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    jsx: true,
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json'
+},
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
