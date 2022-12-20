167e4c648ef99d91d7a3a8100cd5391fa0368240
Kamil Narkiewicz <kamil099887@gmail.com>
2022-12-20 04:19:40 +0100

    add missing library types

---
+++ src/LibTypes/index.d.ts
@@ -0,0 +1,9 @@
+declare module 'react-native-vector-icons/MaterialIcons' {
+    export default class Icon extends React.Component<IconProps, any> {}
+
+    interface IconProps {
+        name: 'compare-arrows';
+        size: number;
+        color: string;
+    }
+}
